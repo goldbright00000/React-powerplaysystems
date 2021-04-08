6a17a246fc80e35a00dd602cd6e4a6ed27629a7a
Hamza Iqbal <13besehiqbal@seecs.edu.pk>
2021-04-08 13:50:08 +0500

    udpate footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,68 +1,114 @@
-import classes from './Footer.module.scss';
-import logo from '../../assets/logo.png';
-import { Link } from 'react-router-dom';
+import classes from "./Footer.module.scss";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
-import {useMediaQuery} from 'react-responsive';
+import { useMediaQuery } from "react-responsive";
 
+const Footer = (props) => {
+  const isMobile = useMediaQuery({ query: "(max-width: 414px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 768px)" });
+  const isBigScreenTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
-const Footer = props => {
-    const isMobile = useMediaQuery({query: '(max-width: 414px)'});
-    const isTablet = useMediaQuery({query: '(max-width: 768px)'});
-    const isBigScreenTablet = useMediaQuery({query: '(max-width: 1024px)'});
-    
-    return (
-        <footer className={`${classes.__footer} ${props?.isBlack && classes.__style_2}`}>
-            <div className={classes.__container_2}>
-                {
-                    props?.logoOnly ?
-                    <div className={classes.__logo_center}>
-                        <img src={logo} alt='' className={`${classes.__logo}`} />
-                    </div>
-                    :
-                    <>
-                        <div className={`${classes.__footer_links} `}>
-                            <img src={logo} alt='' className={`${classes.__logo} __center`} />
-                            {/* <img src={logo} alt='' className={`${classes.__logo} __hide-on-small`} /> */}
-                            {
-                                !isMobile && !isTablet && !isBigScreenTablet
-                                ?
-                                <>
-                                    <div className='__hide-on-small'>Legal Privacy</div>
-                                    <div className='__hide-on-small'>Company</div>
-                                    <Link to='/terms'>Terms of use</Link>
-                                    <Link to='/faqs' className={classes.elm_3}>FAQs</Link>
-                                    <Link to='/my-account' className={classes.elm_1}>My Account</Link>
-                                    <Link to='/privacy'>Privacy</Link>
-                                    <Link to='/trust-and-safety' className={classes.elm_4}>Trust & Safety </Link>
-                                    <Link to='/sponer-contest' className={[classes.elm_2, classes.__add_border]} >Sponsor a contest</Link>
-                                    <Link to='/account-security' className={classes.__add_border}>Account Security</Link>
-                                    <Link to='/about-us' className={[classes.elm_5, classes.__add_border]}>About Us</Link>
-                                </>
-                                :
-                                <>
-                                    <Link to='/my-account' className={classes.elm_1}>My Account</Link>
-                                    <Link to='/sponer-contest' className={[classes.elm_2, classes.__add_border]} >Sponsor a contest</Link>
-                                    <hr className={classes.__hr} />
-                                    <Link to='/faqs' className={classes.elm_3}>FAQs</Link>
-                                    <Link to='/trust-and-safety' className={classes.elm_4}>Trust & Safety </Link>
-                                    <Link to='/about-us' className={[classes.elm_5, classes.__add_border]}>About Us</Link>
-                                    <hr className={classes.__hr} />
-                                    <Link to='/terms'>Terms of use</Link>
-                                    <Link to='/privacy'>Privacy</Link>
-                                    <Link to='/account-security' className={classes.__add_border}>Account Security</Link>
-                                    <hr className={classes.__hr} />
-                                </>
-                            }
-                        </div>
-                        <div className={`${classes.__mediam} __center __mt-3`}>
-                            Copyright © 2021 PowerPlay Systems Inc. - All Rights Reserved
-                        </div>
-                    </>
-                }
-                
+  return (
+    <footer
+      className={`${classes.__footer} ${props?.isBlack && classes.__style_2}`}
+    >
+      <div className={classes.__container_2}>
+        {props?.logoOnly ? (
+          <div className={classes.__logo_center}>
+            <img src={logo} alt="" className={`${classes.__logo}`} />
+          </div>
+        ) : (
+          <>
+            <div className={`${classes.__footer_content}`}>
+              <div className={`${classes.__footer_logo}`}>
+                <img src={logo} alt="" />
+              </div>
+              <div className={`${classes.__footer_links} `}>
+                {!isMobile && !isTablet && !isBigScreenTablet ? (
+                  <>
+                    <div className="__hide-on-small">Esentials</div>
+                    <div className="__hide-on-small">Legal Privacy</div>
+                    <div className="__hide-on-small">Company</div>
+                    <Link to="/my-account" className={classes.elm_1}>
+                      My Account
+                    </Link>
+                    <Link to="/terms">Terms of use</Link>
+                    <Link to="/faqs" className={classes.elm_3}>
+                      FAQs
+                    </Link>
+                    <Link to="/power-center" className={classes.elm_3}>
+                      Power Center
+                    </Link>
+                    <Link to="/privacy">Privacy</Link>
+                    <Link to="/trust-and-safety" className={classes.elm_4}>
+                      Trust & Safety{" "}
+                    </Link>
+                    <Link
+                      to="/sponer-contest"
+                      className={[classes.elm_2, classes.__add_border]}
+                    >
+                      Responsible Gaming
+                    </Link>
+                    <Link
+                      to="/account-security"
+                      className={classes.__add_border}
+                    >
+                      Account Security
+                    </Link>
+                    <Link
+                      to="/contact-us"
+                      className={[classes.elm_5, classes.__add_border]}
+                    >
+                      Contact Us
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link to="/my-account" className={classes.elm_1}>
+                      My Account
+                    </Link>
+                    <Link
+                      to="/sponer-contest"
+                      className={[classes.elm_2, classes.__add_border]}
+                    >
+                      Sponsor a contest
+                    </Link>
+                    <hr className={classes.__hr} />
+                    <Link to="/faqs" className={classes.elm_3}>
+                      FAQs
+                    </Link>
+                    <Link to="/trust-and-safety" className={classes.elm_4}>
+                      Trust & Safety{" "}
+                    </Link>
+                    <Link
+                      to="/about-us"
+                      className={[classes.elm_5, classes.__add_border]}
+                    >
+                      About Us
+                    </Link>
+                    <hr className={classes.__hr} />
+                    <Link to="/terms">Terms of use</Link>
+                    <Link to="/privacy">Privacy</Link>
+                    <Link
+                      to="/account-security"
+                      className={classes.__add_border}
+                    >
+                      Account Security
+                    </Link>
+                    <hr className={classes.__hr} />
+                  </>
+                )}
+              </div>
             </div>
-        </footer>
-    );
-}           
+            <div className={`${classes.__mediam} __center __mt-3`}>
+              Copyright © 2021 PowerPlay Systems Inc. - All Rights Reserved
+            </div>
+          </>
+        )}
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
