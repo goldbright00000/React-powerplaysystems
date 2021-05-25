2b87301ef2ba28bb8b78e1102e75a8592b03afc6
Malik Ali Haider Awan <ali114_haider@outlook.com>
2021-05-25 19:45:43 +0500

    bug fix

--- src/components/ResultsInfoComponent/index.js
+++ src/components/ResultsInfoComponent/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Accordian from "../Accordian";
-import Power from '../../assets/power-play-sidebar-icon.png';
+import Power from "../../assets/power-play-sidebar-icon.png";
 import Token from "../../assets/points-collected.png";
 import Bitcoin from "../../assets/bitcoin.png";
 import Ethereum from "../../assets/ethereum.png";
@@ -17,7 +17,7 @@ const data = [
     iconWithTitle: Token,
   },
   {
-    cashTitle: "Total Tokens: ",
+    cashTitle: "Total : ",
     cash: "5,000",
     dataTitle: "Power Token Prizes",
     type: "tokenBalance",
@@ -25,18 +25,18 @@ const data = [
     iconWithTitle: Power,
   },
   {
-    cashTitle: "",
+    cashTitle: "Total: ",
     cash: "$4500",
     dataTitle: "Bitcoin Prizes",
-    type: "tokenBalance",
+    type: "btcBalance",
     balanceType: "token",
     iconWithTitle: Bitcoin,
   },
   {
-    cashTitle: "",
+    cashTitle: "Total: ",
     cash: "$4500",
     dataTitle: "Ethereum Prizes",
-    type: "tokenBalance",
+    type: "ethBalance",
     balanceType: "token",
     iconWithTitle: Ethereum,
   },
