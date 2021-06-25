3b25c0bbe74e719551be5742fdac2592d57ce491
Sulaiman <devsulaiman3@gmail.com>
2021-06-25 09:02:46 +0500

    APi

--- src/actions/powerCenterActions.js
+++ src/actions/powerCenterActions.js
@@ -6,8 +6,9 @@ export const GET_ALL_POWER_GAMES = "GET_ALL_POWER_GAMES";
 
 export function getAllGames() {
   return (dispatch) => {
+    const api = `${process.env.REACT_APP_API_URL}/${URLS.GAMES.ALL_POWER_GAMES}`;
     axios
-      .get(`${process.env.REACT_APP_API_URL}/${URLS.GAMES.ALL_POWER_GAMES}`)
+      .get(api)
       .then((res) => {
         dispatch({
           type: GET_ALL_POWER_GAMES,
