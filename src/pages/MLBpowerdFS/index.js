53426c8d115b43ab8024a30c52f1c4e2226b7112
Sulaiman <devsulaiman3@gmail.com>
2021-07-07 20:40:40 +0500

    stat and filter fixes

--- src/pages/MLBpowerdFS/index.js
+++ src/pages/MLBpowerdFS/index.js
@@ -387,8 +387,9 @@ function MLBPowerdFs(props) {
 
     //selected players
     const _playersList = [...playerList];
+    const selectionId = `${id} - ${matchId}`;
 
-    if (!selected.get(id)) {
+    if (!selected.get(selectionId)) {
       const [_player] = _playersList?.filter((player) => {
         let obj = {};
         if (currentPlayer?.type?.toLocaleLowerCase() === D) {
@@ -424,7 +425,7 @@ function MLBPowerdFs(props) {
           player.isStarPlayer = currentPlayer?.isStarPlayer;
           _playersList[playerListIndex] = player;
 
-          selected.set(id, !selected.get(id));
+          selected.set(selectionId, !selected.get(selectionId));
           //Star Power Player selection (sidebar)
           if (starPlayerCount < 3 && currentPlayer?.isStarPlayer) {
             _starPlayerCount++;
@@ -435,14 +436,19 @@ function MLBPowerdFs(props) {
     } else {
       let existingPlayerIndex = _playersList?.findIndex((player) => {
         if (currentPlayer?.type?.toLocaleLowerCase() === D) {
-          return player?.team?.team_id === id;
+          return (
+            player?.team?.team_id === id && player?.team?.match_id === matchId
+          );
         } else {
-          return player?.player?.playerId === id;
+          return (
+            player?.player?.playerId === id &&
+            player?.player?.match_id === matchId
+          );
         }
       });
 
       if (existingPlayerIndex !== -1) {
-        selected.set(id, !selected.get(id));
+        selected.set(selectionId, !selected.get(selectionId));
         if (
           starPlayerCount > 0 &&
           _playersList[existingPlayerIndex].isStarPlayer
@@ -512,16 +518,19 @@ function MLBPowerdFs(props) {
     );
     const filter = _selectedFilter;
     let _remaining = filter?.remaining;
-    let id = type === D ? player?.team_id : player?.playerId;
+    const id = type === D ? player?.team_id : player?.playerId;
+    const matchId = player?.match_id;
+
+    const selectionId = `${id} - ${matchId}`;
 
     if (_remaining > 0) {
-      if (!!!selected.get(id)) _remaining -= 1;
+      if (!!!selected.get(selectionId)) _remaining -= 1;
       else if (_remaining < 2) _remaining += 1;
       if (_remaining <= 0) {
         _remaining = 0;
         setSelectedFilter(filter);
       }
-    } else if (!!selected.get(id) && _remaining < 2) {
+    } else if (!!selected.get(selectionId) && _remaining < 2) {
       _remaining++;
     } else {
       setSelectedFilter(_selectedFilter);
@@ -886,7 +895,11 @@ function MLBPowerdFs(props) {
                             {selectedFilter?.title === D ? (
                               <SportsTeamSelectionCard
                                 item={item}
-                                isSelected={!!selected.get(item.team_id)}
+                                isSelected={
+                                  !!selected.get(
+                                    `${item.team_id} - ${item?.match_id}`
+                                  )
+                                }
                                 key={item?.team_id + " - " + item?.match_id}
                                 onSelectDeselect={onPlayerSelectDeselect}
                                 disabled={
@@ -900,7 +913,11 @@ function MLBPowerdFs(props) {
                               <>
                                 <SelectionCard3
                                   player={item}
-                                  isSelected={!!selected.get(item.playerId)}
+                                  isSelected={
+                                    !!selected.get(
+                                      `${item.playerId} - ${item?.match_id}`
+                                    )
+                                  }
                                   key={item.playerId + " - " + item?.match_id}
                                   loading={loading}
                                   onSelectDeselect={onPlayerSelectDeselect}
