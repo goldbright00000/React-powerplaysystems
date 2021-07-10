93c2b54e5b634d721d4a009a174c54c692b0e014
Sulaiman <devsulaiman3@gmail.com>
2021-07-10 09:28:06 +0500

    mlb live updates of points

--- src/components/SportsLiveCard/RenderMLBPlayerStats.js
+++ src/components/SportsLiveCard/RenderMLBPlayerStats.js
@@ -29,18 +29,18 @@ function RenderMLBPlayerStats(props) {
 
   const {
     active: isHitterActive = false,
-    bat_hand: hBatHand = "R",
-    current_position: hCurrentPos = "OF",
-    current_team: hCurrenTeam = 11266,
-    datafeed_id: hDataFeedId = "0f626ddc-1c79-43af-9407-12aa1381d420",
-    height: hHeight = "72",
+    bat_hand: hBatHand = "",
+    current_position: hCurrentPos = "",
+    current_team: hCurrenTeam = 0,
+    datafeed_id: hDataFeedId = "",
+    height: hHeight = "",
     is_injured: isHInjured = true,
-    jersey_number: jJersyNumber = 21,
-    name: hitterName = "Austin Hays",
-    player_id: hId = 10885,
-    primary_position: hPrimaryPos = "LF",
-    throw_hand: hThrowHand = "R",
-    type: hitterType = "OF",
+    jersey_number: jJersyNumber = 0,
+    name: hitterName = "",
+    player_id: hId = 0,
+    primary_position: hPrimaryPos = "",
+    throw_hand: hThrowHand = "",
+    type: hitterType = "",
     mlb_player_stats: hitterStats = [],
   } = hitter || {};
 
@@ -68,19 +68,19 @@ function RenderMLBPlayerStats(props) {
   } = hitterStats[0] || {};
 
   const {
-    active: isPittcherActive = true,
-    bat_hand: pBatHand = "R",
-    current_position: pCurrentPos = "P",
-    current_team: pCurrentTeam = 11288,
-    datafeed_id: pDataFeedId = "51f6f215-403e-43c9-8647-c0fb8a36af47",
-    height: pHeight = "74",
+    active: isPittcherActive = false,
+    bat_hand: pBatHand = "",
+    current_position: pCurrentPos = "",
+    current_team: pCurrentTeam = 0,
+    datafeed_id: pDataFeedId = "",
+    height: pHeight = "",
     is_injured: isPInjured = false,
-    jersey_number: pJersyNumber = 55,
-    name: pitcherName = "Ryan Pressly",
-    player_id: pId = 11294,
-    primary_position: pPrimaryPos = "RP",
-    throw_hand: pThrowHand = "R",
-    type: pType = "P",
+    jersey_number: pJersyNumber = 0,
+    name: pitcherName = "",
+    player_id: pId = 0,
+    primary_position: pPrimaryPos = "",
+    throw_hand: pThrowHand = "",
+    type: pType = "",
     mlb_player_stats: pitcherStats = [],
   } = pitcher || {};
 
@@ -280,7 +280,7 @@ function RenderMLBPlayerStats(props) {
               </p>
             </div>
             <span>
-              {batting_average} | {0}
+              {hbBA} | {hHits}/{0} | {hStrikeOuts} | {hBOB}
             </span>
           </div>
         )}
@@ -293,7 +293,7 @@ function RenderMLBPlayerStats(props) {
                 {formatName(pitcherName)}
               </p>
             </div>
-            <span>{0}</span>
+            <span>ERA: {pERA}</span>
           </div>
         )}
       </div>
