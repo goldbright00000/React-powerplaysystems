966ba6d06393a7ecbfb159644d9cf8e03b17b673
development <development0264@gmail.com>
2021-08-11 19:19:00 +0530

    Remove package

--- src/components/ZumPayment/PaymentFrame.js
+++ src/components/ZumPayment/PaymentFrame.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import MyUserPay from 'myuserpay';
 
 import styles from "./paymentFrame.module.scss";
 
