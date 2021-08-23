bc76c45fb08f41b9aada5d7c145d86ca67df8a82
Sulaiman <devsulaiman3@gmail.com>
2021-08-23 21:56:22 +0500

    Merge branch 'test' of github.com:PowerplaySystems/PowerplayGames into feat/sulaiman

--- src/pages/UsersPaymentGateway/UsersGateway.js
+++ src/pages/UsersPaymentGateway/UsersGateway.js
@@ -1,21 +1,22 @@
 import React, { useEffect } from "react";
-import * as $ from 'jquery';
-import { useHistory } from 'react-router-dom'
+import * as $ from "jquery";
+import { useHistory } from "react-router-dom";
 
 import { getLocalStorage, redirectTo } from "../../utility/shared";
 import { CONSTANTS } from "../../utility/constants";
 import { socket } from "../../config/server_connection";
 
-import './userGateway.css';
+import "./userGateway.css";
 
 let _socket = null;
 
 const UsersGateway = (props) => {
+  const history = useHistory();
+  const {
+    location: { state },
+  } = props;
 
-  const history = useHistory()
-  const { location: { state } } = props
-
-  const user_id = getLocalStorage('PERSONA_USER_ID')
+  const user_id = getLocalStorage("PERSONA_USER_ID");
   const token = getLocalStorage(CONSTANTS.LOCAL_STORAGE_KEYS.USER);
 
   useEffect(() => {
@@ -44,7 +45,7 @@ const UsersGateway = (props) => {
     document.body.appendChild(script);
 
     setTimeout(async () => {
-      $('.myuserPay-Paybutton').click();
+      $(".myuserPay-Paybutton").click();
     }, 1000);
 
     return () => {
@@ -54,7 +55,11 @@ const UsersGateway = (props) => {
 
   return (
     <>
-      <form action={`${process.env.REACT_APP_API_URL}/api/v1/users/account/update-myuserpay-balance`} method="post">
+      <form
+        className={"__form__"}
+        action={`${process.env.REACT_APP_API_URL}/api/v1/users/account/update-myuserpay-balance`}
+        method="post"
+      >
         <input type="hidden" name="token" value={`Bearer ${token}`} />
         <input type="hidden" name="amount" value={state.amount} />
         <input type="hidden" name="user_id" value={user_id} />
