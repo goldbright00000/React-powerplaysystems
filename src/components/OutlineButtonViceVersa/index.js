8605d59cd387f4299f24e218cdd63aa04fb4e1f8
Sulaiman <devsulaiman3@gmail.com>
2021-08-23 20:57:38 +0500

    baseball

---
+++ src/components/OutlineButtonViceVersa/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import classes from "./outlineButtonViceVersa.module.scss";
+
+const OutlineButtonViceVersa = (props) => {
+  const {
+    title = "",
+    onClick = () => { },
+    styles = {},
+    icon = "",
+    title0 = "",
+    currency = "",
+  } = props || {};
+
+  return (
+    <button
+      className={classes.__outline_button}
+      onClick={onClick}
+      style={styles}
+    >
+      {icon && <span className={classes.__outline_button_icon}>{icon}</span>}
+      {
+        title0 && (
+          title0
+        )
+      }
+      {
+        currency && (
+          <img
+            src={currency}
+            alt=""
+            height="18"
+          />
+        )
+      }
+      {title}
+    </button >
+  );
+};
+
+export default OutlineButtonViceVersa;
