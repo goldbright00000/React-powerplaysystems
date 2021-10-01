3fad8b2755d0d359092cd9702a648e5c4490bb87
Malik Ali Haider Awan <ali114_haider@outlook.com>
2021-10-01 11:17:10 +0500

    sports live card updated

--- src/components/SportsLiveCard/index.js
+++ src/components/SportsLiveCard/index.js
@@ -651,13 +651,15 @@ function SportsLiveCard(props) {
   const RenderStatPoints = ({}) => (
     <div className={classes.stat_points}>
       <div className={classes.stat_points_container}>
-        <p
-          className={`${classes.stat_points_title} ${
-            largeView && classes.large_view
-          }`}
-        >
-          Stats
-        </p>
+        <Tooltip toolTipContent="Point Generating Plays">
+          <p
+            className={`${classes.stat_points_title} ${
+              largeView && classes.large_view
+            }`}
+          >
+            PGPs
+          </p>
+        </Tooltip>
         <div className={`${classes.stat} ${largeView && classes.large_view}`}>
           {type === "P" ? (
             <>
@@ -724,7 +726,7 @@ function SportsLiveCard(props) {
               largeView && classes.large_view
             }`}
           >
-            Stats
+            <Tooltip toolTipContent="Point Generating Plays">PGPs</Tooltip>
           </p>
           <p className={`${classes.p} ${largeView && classes.large_view}`}>
             G: {goals} | A: {assists}
