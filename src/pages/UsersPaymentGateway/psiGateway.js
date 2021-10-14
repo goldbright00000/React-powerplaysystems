3aacbe6c2c1dbaf805d4c8e300838705a68aa88d
development <development0264@gmail.com>
2021-10-14 18:29:50 +0530

    Minor design fix

--- src/pages/UsersPaymentGateway/psiGateway.js
+++ src/pages/UsersPaymentGateway/psiGateway.js
@@ -6,7 +6,7 @@ import { getLocalStorage, redirectTo } from "../../utility/shared";
 import { CONSTANTS } from "../../utility/constants";
 import { socket } from "../../config/server_connection";
 
-import "./userGateway.css";
+import "./psiGateway.css";
 
 let _socket = null;
 
@@ -55,33 +55,59 @@ const PSiGateway = (props) => {
 
     return (
         <>
-            <form action='https://stagingcheckout.psigate.com/HTMLPost/HTMLMessenger' method="post">
-                MerchantID: <input type="text" name="MerchantID" value="merchantcardcapture200024" /><br />
-                CustomerRefNo: <input type="text" name="CustomerRefNo" value="32323232" /><br />
-                PaymentType: <input type="text" name="PaymentType" value="DB" /><br />
-                TestResult: <input type="text" name="TestResult" value="" /><br />
-                OrderID: <input type="text" name="OrderID" value="" /><br />
-                UserID: <input type="text" name="UserID" value="User1" /><br />
-                CardAction: <input type="text" name="CardAction" value="0" /><br />
-                CardNumber: <input type="text" name="CardNumber" value="2624970525726523" /><br />
-                Email: <input type="text" name="Email" value="someone@somewhere.com" /><br />
-                CardExpMonth: <input type="text" name="CardExpMonth" value="06" /><br />
-                CardExpYear: <input type="text" name="CardExpYear" value="2022" /><br />
-                Tax1: <input type="text" name="Tax1" value="5" /><br />
-                ShippingTotal: <input type="text" name="ShippingTotal" value="6" /><br />
-                SubTotal: <input type="text" name="SubTotal" value="8" /><br />
+            <form action='https://stagingcheckout.psigate.com/HTMLPost/HTMLMessenger' method="post" className="main-form">
+                <div className="form-input">
+                    <p>MerchantID:</p><input type="text" name="MerchantID" value="merchantcardcapture200024" />
+                </div>
+                <div className="form-input">
+                    <p>CustomerRefNo:</p> <input type="text" name="CustomerRefNo" value="32323232" /><br />
+                </div>
+                <div className="form-input">
+                    <p>PaymentType:</p> <input type="text" name="PaymentType" value="DB" /><br />
+                </div>
+                <div className="form-input">
+                    <p>TestResult:</p> <input type="text" name="TestResult" value="" /><br />
+                </div>
+                <div className="form-input">
+                    <p>OrderID:</p> <input type="text" name="OrderID" value="" /><br />
+                </div>
+                <div className="form-input">
+                    <p>UserID:</p> <input type="text" name="UserID" value="User1" /><br />
+                </div>
+                <div className="form-input">
+                    <p>CardAction:</p> <input type="text" name="CardAction" value="0" /><br />
+                </div>
+                <div className="form-input">
+                    <p>CardNumber:</p> <input type="text" name="CardNumber" value="2624970525726523" /><br />
+                </div>
+                <div className="form-input">
+                    <p>Email:</p> <input type="text" name="Email" value="someone@somewhere.com" /><br />
+                </div>
+                <div className="form-input">
+                    <p>CardExpMonth:</p> <input type="text" name="CardExpMonth" value="06" /><br />
+                </div>
+                <div className="form-input">
+                    <p>CardExpYear:</p> <input type="text" name="CardExpYear" value="2022" /><br />
+                </div>
+                <div className="form-input">
+                    <p>Tax1:</p> <input type="text" name="Tax1" value="5" /><br />
+                </div>
+                <div className="form-input">
+                    <p>ShippingTotal:</p> <input type="text" name="ShippingTotal" value="6" /><br />
+                </div>
+                <div className="form-input">
+                    <p>SubTotal:</p> <input type="text" name="SubTotal" value="8" /><br />
+                </div>
 
                 <input type="hidden" name="ThanksURL" value={`${process.env.REACT_APP_API_URL}/api/v1/users/catch-thanks-url`} />
                 <input type="hidden" name="NoThanksURL" value={`${process.env.REACT_APP_API_URL}/api/v1/users/catch-nothanks-url`} />
 
                 {/* <input type="hidden" name="ThanksURL" value="https://stagingcheckout.psigate.com/HTMLPost/generic_nothankyou.jsp" />
-        <input type="hidden" name="NoThanksURL" value="https://stagingcheckout.psigate.com/HTMLPost/generic_nothankyou.jsp" /> */}
-                <input type="submit" value="Buy Now" />
+                <input type="hidden" name="NoThanksURL" value="https://stagingcheckout.psigate.com/HTMLPost/generic_nothankyou.jsp" /> */}
+                <div className="form-btn">
+                    <input type="submit" value="Buy Now" />
+                </div>
             </form>
-
-            <div className="loder">
-                <span className="load"></span>
-            </div>
         </>
     );
 };
