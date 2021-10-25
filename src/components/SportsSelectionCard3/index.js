c81bb9d9881c80f17d4a67a1543bf897abcaaf94
Malik Ali Haider Awan <ali114_haider@outlook.com>
2021-10-25 21:11:15 +0500

    nhl sports selection card updated

--- src/components/SportsSelectionCard3/index.js
+++ src/components/SportsSelectionCard3/index.js
@@ -61,12 +61,30 @@ function SportsSelectionCard3(props) {
     is_injured = false,
     isInjured = false,
     match = {},
-    seasons = {},
+    seasons = [],
+    team = {},
   } = player || {};
 
   const { home = {}, away = {}, scheduled, venue = {} } = match || {};
-  const { teams = [] } = seasons[0] || {};
-  const { statistics = {} } = teams[0] || {};
+
+  let [teams, setTeams] = useState([]);
+  let [statistics, setStatistics] = useState([]);
+
+  useEffect(() => {
+    if (seasons) {
+      if (seasons.length > 0) {
+        setTeams(seasons[seasons.length - 1]);
+      }
+    }
+    if (teams) {
+      for (let item of teams) {
+        if (item.id == team.id) {
+          setStatistics(item.statistics);
+          break;
+        }
+      }
+    }
+  }, [player]);
 
   const checkIfIsStarPlayer = () => {
     if (pageType === PAGE_TYPES.NHL) {
