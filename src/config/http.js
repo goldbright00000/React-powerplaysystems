43aa2bf2021ee39346ebf77c74d1b30a5a69ee32
i-Verve <mukesh.sachdev@ifuturz.com>
2021-12-01 19:50:50 +0530

    changes

--- src/config/http.js
+++ src/config/http.js
@@ -6,7 +6,7 @@ import { CONSTANTS } from "../utility/constants";
 
 const http = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/api/v1` /*|| apiUrl*/,
-  //timeout: 60000, //60 seconds timeout
+  timeout: 60000, //60 seconds timeout
   timeoutErrorMessage: "Request time out, please try again later",
   maxRedirects: 3,
 });
