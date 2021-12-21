bd6a4b0195d299ca0b08f4fd618c24e75bc6ce57
bhavinsen <bs.globalia@gmail.com>
2021-12-21 13:01:22 +0530

    pointSystem showing on the flip cards

--- src/components/PowerCenterCardDetails/PointSystem.js
+++ src/components/PowerCenterCardDetails/PointSystem.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+import { useHistory } from 'react-router-dom';
 import classes from "./pointSystem.module.scss";
 import _ from "underscore";
 
 const PointSystem = (props) => {
+  const history = useHistory();
   const { title = "", isMobile = false, PointsSystem = [] } = props || {};
   const groupedPoints = _.groupBy(PointsSystem, 'type');
   const typeOne = Object.keys(groupedPoints);
@@ -10,8 +12,10 @@ const PointSystem = (props) => {
   return (
     <div className={`${classes.__point_system}`}>
       <>
+      {console.log("hhhhh",history.location.pathname)}
         <p className={classes.__point_system_title}>Point System</p>
-        {typeOne.map((d, i) => {
+        {history.location.pathname === "/power-center" ? 
+        typeOne.map((d, i) => {
           return (
             <>
               <div className={classes.__point_system_heading}>{d}</div>
@@ -43,6 +47,7 @@ const PointSystem = (props) => {
                       </>
                     );
                   })}
+                  
                   {/* {Object.entries(PointsSystem[d]).forEach((item, value) => {
                     return (
                       <>
@@ -98,9 +103,201 @@ const PointSystem = (props) => {
                   })}
                 </div> */}
               </div>
+              
             </>
           );
-        })}
+        })
+      :
+      <>
+        {/* Team -D  */}
+        <div  className={`${classes.__point_system_heading} ${classes.__point_game_handling}`}>{Object.keys(PointsSystem)[2]}
+          <div className={classes.__points_grid_data2}>
+              <div className={classes.__points_grid_data1}>
+                <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.teamD)[1]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                              {PointsSystem.teamD.shotsAgainst}
+                            </p>
+                          </div>
+                          </div>
+                          <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.teamD)[0]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                              {PointsSystem.teamD.goalsAgainst}
+                            </p>
+                          </div>
+                        </div>
+                </div>
+          </div>
+        </div>
+
+        {/* goalie  */}
+        <div className={`${classes.__point_system_heading} ${classes.__point_game_handling}`}>{Object.keys(PointsSystem)[0]}
+          <div className={classes.__points_grid_data2}>
+              <div className={classes.__points_grid_data1}>
+                <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.goalie)[0]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                             {PointsSystem.goalie.goalsAgainst}
+                            </p>
+                          </div>
+                          </div>
+                          <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.goalie)[1]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                              {PointsSystem.goalie.save}
+                            </p>
+                          </div>
+                        </div>
+                </div>
+          </div>
+        </div>
+
+      {/* skater */}
+        <div  className={`${classes.__point_system_heading} ${classes.__point_game_handling}`}>{Object.keys(PointsSystem)[1]}
+          <div className={classes.__points_grid_data2}>
+              <div className={classes.__points_grid_data1}>
+                <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.skater)[1]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                              
+                              {PointsSystem.skater.shots}
+                            </p>
+                          </div>
+                          </div>
+                          <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.skater)[4]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                              
+                              {PointsSystem.skater.OTGoal}
+                            </p>
+                          </div>
+                        </div>
+                        <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.skater)[0]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                              
+                              {PointsSystem.skater.goal}
+                            </p>
+                          </div>
+                        </div>
+                        <div className={classes.__point_system_data}>
+                          <div
+                            className={`${classes.__point_system_data_title_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_title} mr-1`}
+                            >
+                              {Object.keys(PointsSystem.skater)[3]}
+                            </p>
+                          </div>
+                          <div
+                            className={`${classes.__point_system_data_value_div}`}
+                          >
+                            <p
+                              className={`${classes.__point_system_data_value} ml-1`}
+                            >
+                              
+                              {PointsSystem.skater.assists}
+                            </p>
+                          </div>
+                      </div>
+                </div>
+          </div>
+        </div>
+       </>
+      }
       </>
     </div>
   );
